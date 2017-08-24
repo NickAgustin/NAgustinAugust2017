0610241734002587ee9a35063318ead4e42d9866
NickAgustin <18agustinn29@millshighsch.org>
2017-08-24 09:30:43 -0700

    Project_2B! Mistakes were made!

---
+++ Project_2B/src/Project_2B.java
@@ -0,0 +1,14 @@
+//Nick Agustin
+/*August 24th, 2017
+ * This class compares output using print and println. 
+ */
+public class Project_2B {
+
+	public static void main(String[] args) {
+		System.out.print("Hi there!");
+		System.out.println("Hi there");
+		System.out.print("I need Healing!");
+
+	}
+
+}
