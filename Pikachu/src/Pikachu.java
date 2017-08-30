d9caf1c9cbffb7f0bc187df951a397c8c492f27f
NickAgustin <18agustinn29@millshighsch.org>
2017-08-30 10:37:07 -0700

    Hi Pikachu!

---
+++ Pikachu/src/Pikachu.java
@@ -0,0 +1,13 @@
+//NickAgustin
+//August 30th
+//Pikachu
+public class Pikachu {
+
+	public static void main(String[] args) {
+		System.out.println("\tPikachu welcomes you to the world of Pokemon!");
+		System.out.println("\t\t\t  (\\__/)");
+		System.out.println("\t\t\t  (o^-^)");
+		System.out.println("\t\t\t z(\")(\")");
+	}
+
+}
