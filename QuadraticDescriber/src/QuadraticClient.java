5ab6b854b1767a13a967c94d013c59c9dbade2cb
NickAgustin <18agustinn29@millshighsch.org>
2017-10-18 10:31:10 -0700

    PROGRESS

--- QuadraticDescriber/src/QuadraticClient.java
+++ QuadraticDescriber/src/QuadraticClient.java
@@ -1,13 +1,15 @@
 import java.util.Scanner;
 
 public class QuadraticClient {
-	
+	Scanner userInput = new Scanner(System.in);
 	public static void main(String[] args) {
-		Scanner userInput = new Scanner(System.in);
-		do(
-				System.out.println(Quadratic.quadDescriber(a, b, c));
-				)
+	}
 		
+		public static String quadDescriber(double a, double b, double c) {
+			System.out.println(Quadratic.quadForm);
+			{
+			}
+		return String; 
 	}
 	
 }
