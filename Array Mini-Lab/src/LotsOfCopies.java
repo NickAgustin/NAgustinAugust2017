824c420aea2a24c483bce791359700121972ec74
NickAgustin <18agustinn29@millshighsch.org>
2017-10-30 09:52:52 -0700

    WORK ON!

---
+++ Array Mini-Lab/src/LotsOfCopies.java
@@ -0,0 +1,22 @@
+
+public class LotsOfCopies 
+{
+
+	public static void main(String[] args) 
+	{
+		int num = 7;
+		String strMain = "APCS";
+		int[] arrMain = {1 ,2, 3, 4, 5};
+		LotsOfCopies.changeMe(num, strMain, arrMain);
+	}
+	public static void changeMe(int x, String str, int[] arr)
+	{
+		x = 100;
+		str = "One Hundred";
+		arr[0] = 101;
+		arr[1] = 102;
+		arr[2] = 103;
+		arr[3] = 104;
+		arr[4] = 105;		
+	}
+}
